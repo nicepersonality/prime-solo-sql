e520d391f687434abbede414baaa585d92e33ffc
Lilith Bentley <sbentley@gmail.com>
2019-09-10 16:46:53 -0500

    Answer Stretch Goal questions.

--- database.sql
+++ database.sql
@@ -36,9 +36,19 @@ WHERE ("city" = 'miami' OR "city" = 'phoenix') AND "transactions_completed" < 5;
 -- Stretch Goal Questions (word problems)
 
 -- 1. Anthony moved to Santa Fe.
+UPDATE "accounts"
+SET "city" = 'santa fe'
+WHERE "username" = 'anthony';
 
 -- 2. Grace closed her account.
+DELETE FROM "accounts"
+WHERE "username" = 'grace';
 
 -- 3. Travis made a withdrawl of $20,000. What's their new balance? NOTE: Research RETURNING
+UPDATE "accounts"
+SET "account_balance" = "account_balance" - 20000
+WHERE "username" = 'travis';
 
 -- 4. The Bank needs to track last names. NOTE: Research ALTER TABLE https://www.postgresql.org/docs/10/static/sql-altertable.html
+ALTER TABLE "accounts"
+ADD COLUMN IF NOT EXISTS "last_name" varchar(60);
