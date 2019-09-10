9ccb9e94c836c1f94cada1abd80ce580a7c9200c
Lilith Bentley <sbentley@gmail.com>
2019-09-10 16:12:12 -0500

    Add questions to document.

---
+++ database.sql
@@ -0,0 +1,29 @@
+-- Base Mode Questions
+
+-- 1. How do you get all users from Chicago?
+
+-- 2. How do you get all users with usernames that contain the letter a?
+
+-- 3. The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
+
+-- 4. How do you select all users that have attempted 9 or more transactions?
+
+-- 5. How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+
+-- 6. How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+
+-- 7. How do you get all users with account balances that are more than $100?
+
+-- 8. How do you add a new account?
+
+-- 9. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+
+-- Stretch Goal Questions (word problems)
+
+-- 1. Anthony moved to Santa Fe.
+
+-- 2. Grace closed her account.
+
+-- 3. Travis made a withdrawl of $20,000. What's their new balance? NOTE: Research RETURNING
+
+-- 4. The Bank needs to track last names. NOTE: Research ALTER TABLE https://www.postgresql.org/docs/10/static/sql-altertable.html
